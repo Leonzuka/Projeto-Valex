0eb818da8ad29bcc8e02c90c7ab5d070118c8e5a
Leonzuka <leonardofeitosa789@gmail.com>
2025-02-10 18:12:03 -0300

    Auto-commit: 10/02/2025 18:12:03,20

--- frontend/src/components/LoginCard.tsx
+++ frontend/src/components/LoginCard.tsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import './LoginCard.css';
 
 // Definir a URL base diretamente
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'projeto-valex-production.up.railway.app/api';
 
 interface LoginCardProps {
   type: 'gestor' | 'cooperado';
