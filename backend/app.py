5bc1868c67f47c917752015fb052ed4f8064d26f
Leonzuka <leonardofeitosa789@gmail.com>
2025-02-10 14:08:50 -0300

    Auto-commit: 10/02/2025 14:08:50,75

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
 from models.models import db  # Mudando a importação
--- backend/build.sh
+++ backend/build.sh
@@ -2,17 +2,4 @@
 #!/bin/bash
 # Atualizar pip
 python -m pip install --upgrade pip
-
-# Instalar dependências
 pip install -r requirements.txt
-
-# Remover pasta de migrations se existir
-rm -rf migrations/
-
-# Reinicializar migrations do zero
-export FLASK_APP=app:create_app
-flask db init
-flask db migrate -m "initial migration"
-flask db upgrade
-
-echo "Build completed."
