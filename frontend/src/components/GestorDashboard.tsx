71fe30529104be3286f0909eaec8c3617e7f3d50
Leonzuka <leonardofeitosa789@gmail.com>
2025-02-13 11:12:13 -0300

    Auto-commit: 13/02/2025 11:12:13,07

--- frontend/src/components/GestorDashboard.tsx
+++ frontend/src/components/GestorDashboard.tsx
@@ -84,16 +84,6 @@ const GestorDashboard: React.FC = () => {
     navigate('/');
   };
 
-  const SectionLoading = () => (
-    <div className="animate-pulse">
-      <div className="h-8 bg-gray-200 rounded w-1/3 mb-4"></div>
-      <div className="space-y-3">
-        <div className="h-20 bg-gray-200 rounded"></div>
-        <div className="h-20 bg-gray-200 rounded"></div>
-      </div>
-    </div>
-  );
-
   if (isLoading) {
     return (
       <div className="min-h-screen flex items-center justify-center bg-gray-100">
